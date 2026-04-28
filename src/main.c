b7393a9b7381ed79baff9f66d389655908247b52
joao-lucas-si <joao6lucas2168@gmail.com>
2026-04-28 09:50:55 -0300

    adicionando mensagem

--- src/main.c
+++ src/main.c
@@ -1,6 +1,17 @@
 #include<stdio.h>
 
+
+void mostrarDelimitadores() {
+     for (int i =0; i < 5; i++) {
+        printf("=====");
+    }
+    printf("\n");
+}
+
 int main() {
+    mostrarDelimitadores();
+    printf("   SISTEMA EQUIPE 101   \n");
+    mostrarDelimitadores();
     
 
     
