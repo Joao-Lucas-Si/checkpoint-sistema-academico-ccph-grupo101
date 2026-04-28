ac5bb8a0aeeea001f859011afc0b7ef1d72d7721
EnzoRibeiroDV <profissional.enzo.ribeiro@gmail.com>
2026-04-28 10:06:01 -0300

    adicionando opções

--- src/main.c
+++ src/main.c
@@ -13,6 +13,20 @@ int main() {
     printf("   SISTEMA EQUIPE 101   \n");
     mostrarDelimitadores();
     
+    float nota1, nota2, nota3, nota4;
+
+    int sair = 0;
+
+    while (sair != 1)
+    {
+        printf("1 - Inserir notas");
+        printf("2 - Calcular media");
+        printf("3 - Verificar situacao");
+        printf("4 - Exibir resultado");
+        printf("5 - Calcular derivada");
+        printf("6 - Sair");
+    }
+    
 
     
     return 0;
